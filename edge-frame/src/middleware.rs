11b6a43f50ce8046a9794b9587cad13e1c6cdbea
ivmarkov <ivan.markov@gmail.com>
2023-10-23 12:28:03 +0000

    Reduce the number of features

--- edge-frame/src/middleware.rs
+++ edge-frame/src/middleware.rs
@@ -1,14 +1,25 @@
+use core::cell::RefCell;
 use core::fmt::Debug;
 
-use log::{log, Level};
+extern crate alloc;
+use alloc::rc::Rc;
+
+use log::{log, trace, Level};
+
+use embassy_sync::channel;
+use serde::{de::DeserializeOwned, Serialize};
+
+use futures::stream::{SplitSink, SplitStream};
+use futures::{SinkExt, StreamExt};
+
+use gloo_net::websocket::{futures::WebSocket, Message};
+
+use postcard::to_allocvec;
+
+use wasm_bindgen::JsError;
+use wasm_bindgen_futures::spawn_local;
 
 use yewdux_middleware::*;
-
-#[cfg(feature = "middleware-local")]
-pub use local::*;
-
-#[cfg(feature = "middleware-ws")]
-pub use ws::*;
 
 pub fn log_msg<M, D>(level: Level) -> impl Fn(M, D)
 where
@@ -37,135 +48,91 @@ where
     }
 }
 
-#[cfg(feature = "middleware-local")]
-mod local {
-    use core::cell::RefCell;
-    use core::fmt::Debug;
+#[allow(clippy::await_holding_refcell_ref)]
+pub fn send_local<M>(sender: impl Into<channel::DynamicSender<'static, M>>) -> impl Fn(M)
+where
+    M: Debug + 'static,
+{
+    let sender = Rc::new(RefCell::new(sender.into()));
 
-    extern crate alloc;
-    use alloc::sync::Arc;
-
-    use log::trace;
-
-    use wasm_bindgen_futures::spawn_local;
-
-    use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel, mutex::Mutex};
-
-    use yewdux_middleware::*;
-
-    pub fn send<M>(sender: impl Into<channel::DynamicSender<'static, M>>) -> impl Fn(M)
-    where
-        M: Debug + 'static,
-    {
-        let sender = Arc::new(Mutex::<CriticalSectionRawMutex, _>::new(sender.into()));
-
-        move |msg| {
-            let sender = sender.clone();
-
-            spawn_local(async move {
-                trace!("Sending request: {:?}", msg);
-
-                let guard = sender.lock().await;
-
-                guard.send(msg).await;
-            });
-        }
-    }
-
-    pub fn receive<M>(receiver: impl Into<channel::DynamicReceiver<'static, M>>)
-    where
-        M: Debug + 'static,
-    {
-        let receiver = receiver.into();
+    move |msg| {
+        let sender = sender.clone();
 
         spawn_local(async move {
-            loop {
-                let event = receiver.receive().await;
-                trace!("Received event: {:?}", event);
+            trace!("Sending request: {:?}", msg);
 
-                dispatch::invoke(event);
-            }
+            let guard = sender.borrow_mut();
+
+            guard.send(msg).await;
         });
     }
 }
 
-#[cfg(feature = "middleware-ws")]
-mod ws {
-    use core::cell::RefCell;
-    use core::fmt::Debug;
+pub fn receive_local<M>(receiver: impl Into<channel::DynamicReceiver<'static, M>>)
+where
+    M: Debug + 'static,
+{
+    let receiver = receiver.into();
 
-    extern crate alloc;
-    use alloc::sync::Arc;
+    spawn_local(async move {
+        loop {
+            let event = receiver.receive().await;
+            trace!("Received event: {:?}", event);
 
-    use serde::{de::DeserializeOwned, Serialize};
-
-    use log::trace;
-
-    use futures::stream::{SplitSink, SplitStream};
-    use futures::{SinkExt, StreamExt};
-
-    use gloo_net::websocket::{futures::WebSocket, Message};
-
-    use postcard::to_allocvec;
-
-    use wasm_bindgen::JsError;
-    use wasm_bindgen_futures::spawn_local;
-
-    use yewdux_middleware::dispatch;
-
-    use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
-
-    pub fn open(
-        ws_endpoint: &str,
-    ) -> Result<(SplitSink<WebSocket, Message>, SplitStream<WebSocket>), JsError> {
-        open_url(&format!(
-            "ws://{}/{}",
-            web_sys::window().unwrap().location().host().unwrap(),
-            ws_endpoint,
-        ))
-    }
-
-    fn open_url(
-        url: &str,
-    ) -> Result<(SplitSink<WebSocket, Message>, SplitStream<WebSocket>), JsError> {
-        let ws = WebSocket::open(url)?;
-
-        Ok(ws.split())
-    }
-
-    pub fn send<M>(sender: SplitSink<WebSocket, Message>) -> impl Fn(M)
-    where
-        M: Serialize + Debug + 'static,
-    {
-        let sender = Arc::new(Mutex::<CriticalSectionRawMutex, _>::new(sender));
-
-        move |msg| {
-            let sender = sender.clone();
-
-            spawn_local(async move {
-                trace!("Sending request: {:?}", msg);
-
-                let mut guard = sender.lock().await;
-
-                guard
-                    .send(Message::Bytes(to_allocvec(&msg).unwrap()))
-                    .await
-                    .unwrap();
-            });
+            dispatch::invoke(event);
         }
-    }
+    });
+}
 
-    pub fn receive<M>(mut receiver: SplitStream<WebSocket>)
-    where
-        M: DeserializeOwned + Debug + 'static,
-    {
+pub fn open(
+    ws_endpoint: &str,
+) -> Result<(SplitSink<WebSocket, Message>, SplitStream<WebSocket>), JsError> {
+    open_url(&format!(
+        "ws://{}/{}",
+        web_sys::window().unwrap().location().host().unwrap(),
+        ws_endpoint,
+    ))
+}
+
+fn open_url(url: &str) -> Result<(SplitSink<WebSocket, Message>, SplitStream<WebSocket>), JsError> {
+    let ws = WebSocket::open(url)?;
+
+    Ok(ws.split())
+}
+
+#[allow(clippy::await_holding_refcell_ref)]
+pub fn send<M>(sender: SplitSink<WebSocket, Message>) -> impl Fn(M)
+where
+    M: Serialize + Debug + 'static,
+{
+    let sender = Rc::new(RefCell::new(sender));
+
+    move |msg| {
+        let sender = sender.clone();
+
         spawn_local(async move {
-            loop {
-                let event = receiver.next().await.unwrap().unwrap();
-                trace!("Received event: {:?}", event);
+            trace!("Sending request: {:?}", msg);
 
-                dispatch::invoke(event);
-            }
+            let mut guard = sender.borrow_mut();
+
+            guard
+                .send(Message::Bytes(to_allocvec(&msg).unwrap()))
+                .await
+                .unwrap();
         });
     }
+}
+
+pub fn receive<M>(mut receiver: SplitStream<WebSocket>)
+where
+    M: DeserializeOwned + Debug + 'static,
+{
+    spawn_local(async move {
+        loop {
+            let event = receiver.next().await.unwrap().unwrap();
+            trace!("Received event: {:?}", event);
+
+            dispatch::invoke(event);
+        }
+    });
 }
